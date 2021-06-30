0b88d8c7b9500cf191a395077f30d3221a707797
Vaida <93vaida@gmail.com>
2021-06-30 16:01:42 +0200

    updated Card component test to match code changes

--- frontend/src/components/card/Card.test.js
+++ frontend/src/components/card/Card.test.js
@@ -1,41 +1,44 @@
-import Card from './Card';
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event'
+import Card from "./Card";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-test('Card renders closed initially', () => {
+test("Card renders closed initially", () => {
   const cardTitle = `Test_${Math.random().toString().slice(2)}`;
   const cardDescription = `Description_${Math.random().toString().slice(2)}`;
-  const cardElement = render(<Card title={cardTitle}> {cardDescription} </Card>);
+  const cardElement = render(
+    <Card title={cardTitle} description={cardDescription} />
+  );
   // Title
   const titleElement = screen.getByText(cardTitle);
   expect(titleElement).toBeInTheDocument();
 
   // Description
-  const descriptionElement = screen.queryByText(cardDescription);
-  expect(descriptionElement).not.toBeInTheDocument();
+  const descriptionElement = screen.getByText(cardDescription);
+  expect(descriptionElement).not.toHaveClass("card-expanded");
 
   // Down Arrow:
-  const downArrowElement = cardElement.getByTestId('DownArrow');
+  const downArrowElement = cardElement.getByTestId("DownArrow");
   expect(downArrowElement).toBeInTheDocument();
 });
 
-test('Card can be opened by clicking the arrow', () => {
+test("Card can be opened by clicking the arrow", () => {
   const cardTitle = `Test_${Math.random().toString().slice(2)}`;
   const cardDescription = `Description_${Math.random().toString().slice(2)}`;
-  const cardElement = render(<Card title={cardTitle}> {cardDescription} </Card>);
-  
+  const cardElement = render(
+    <Card title={cardTitle} description={cardDescription} />
+  );
+
   // Down Arrow:
-  const downArrowElement = cardElement.getByTestId('DownArrow');
+  const downArrowElement = cardElement.getByTestId("DownArrow");
   expect(downArrowElement).toBeInTheDocument();
-  
+
   userEvent.click(downArrowElement, { button: 1 });
-  
+
   // Up Arrow:
-  const upArrowElement = cardElement.getByTestId('UpArrow');
+  const upArrowElement = cardElement.getByTestId("UpArrow");
   expect(upArrowElement).toBeInTheDocument();
 
   // Description
   const descriptionElement = screen.getByText(cardDescription);
-  expect(descriptionElement).toBeInTheDocument();
-
+  expect(descriptionElement).toHaveClass("card-expanded");
 });
