9b3fd431473d4d9e9be88072fca498def38bd56a
Vaida <93vaida@gmail.com>
2021-07-01 09:37:49 +0200

    Added testing id

--- frontend/src/components/CardSearch/CardSearch.jsx
+++ frontend/src/components/CardSearch/CardSearch.jsx
@@ -11,18 +11,24 @@ const CardSearch = ({ cards }) => {
   const searchChange = e => setSearch(e.target.value);
 
   const displayedCards = cards.filter(
-    card => !search || (card.title.toLowerCase().startsWith(search.toLowerCase()))
+    card => !search || card.title.toLowerCase().startsWith(search.toLowerCase())
   );
+
+  const generatedKey = () => Math.floor(Math.random() * 100000);
 
   return (
     <>
       <div className="card-container-searchbar center-items">
-        <Input placeholder="Filter Cards" onChange={searchChange} />
+        <Input
+          placeholder="Filter Cards"
+          onChange={searchChange}
+          data-testid="search-input"
+        />
       </div>
-      <div className="card-container">
+      <div className="card-container" data-testid="card-container">
         {displayedCards.map(card => (
           <Card
-            key={card.description}
+            key={generatedKey()}
             title={card.title}
             description={card.description}
           />
