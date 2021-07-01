7e64e93feafae33d04acb7e904e164ba34474309
Vaida <93vaida@gmail.com>
2021-07-01 10:56:52 +0200

    if there is no todo item with provided id, throw an error

--- backend/src/dataStore.js
+++ backend/src/dataStore.js
@@ -1,28 +1,29 @@
 // We're not setting up a database for this test, but this file "emulates" one.
 // You can ignore this file for the technical test
 const DATA = {
-    todos: [
-        {
-            title: 'Something',
-            created : (new Date(Date.now() - (7*24*60*60*1000))).toISOString() // Created a week ago
-        }
-    ]
+  todos: [
+    {
+      title: "Something",
+      created: new Date(Date.now() - 7 * 24 * 60 * 60 * 1000).toISOString(), // Created a week ago
+    },
+  ],
 };
 
-
 function getTodoById(id) {
-    if(!Number.isInteger(id)) {
-        throw new TypeError('Id must be integer');
-    }
+  if (!Number.isInteger(+id)) {
+    throw new TypeError("Id must be integer");
+  } else if (+id > DATA.todos.length - 1) {
+    throw new Error("There is no such todo item");
+  }
 
-    return DATA.todos[id] ? JSON.parse(JSON.stringify(DATA.todos[id])): null;
+  return JSON.parse(JSON.stringify(DATA.todos[id]));
 }
 
 function addTodo(data) {
-    DATA.todos.push(data);
+  DATA.todos.push(data);
 }
 
 module.exports = {
-    getTodoById,
-    addTodo
+  getTodoById,
+  addTodo,
 };
