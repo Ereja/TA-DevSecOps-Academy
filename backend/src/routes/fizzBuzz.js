73486c47f75e8ff12551c29d06c99f3fdbea9ca2
Vaida <93vaida@gmail.com>
2021-07-01 14:40:39 +0200

    minor bug fixes

--- backend/src/routes/fizzBuzz.js
+++ backend/src/routes/fizzBuzz.js
@@ -7,7 +7,6 @@ FizzBuzzRouter.post("/:number", (req, res) => {
   //getting a number from URL
   const { number } = req.params;
 
-  //by default req.params will return a string, we try to convert that string to a number, if it fails - user will get error message, if it passes(for example - five will fail, 5 - will pass), they can play the fizzbuzz
   if (!Number(+number)) {
     res.status(400).send("To play FizzBuzz you must enter an integer");
   } else {
